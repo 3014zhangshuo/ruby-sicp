5365d34813bc41a09d69eda1f72be92f39afd355
3014zhangshuo <3014zhangshuo@gmail.com>
2019-09-24 09:31:26 +0800

    init simple language

---
+++ chap2/simple_language.rb
@@ -0,0 +1,153 @@
+class Number < Struct.new(:value)
+  def to_s
+    value.to_s
+  end
+
+  def inspect
+    "<<#{self}>>"
+  end
+
+  def reducible?
+    false
+  end
+end
+
+class Boolean < Struct.new(:value)
+  def to_s
+    value.to_s
+  end
+
+  def inspect
+    "<<#{self}>>"
+  end
+
+  def reducible?
+    false
+  end
+end
+
+class Add < Struct.new(:left, :right)
+  def reduce(environment)
+    if left.reducible?
+      Add.new(left.reduce(environment), right)
+    elsif right.reducible?
+      Add.new(left, right.reduce(environment))
+    else
+      Number.new(left.value + right.value)
+    end
+  end
+
+  def to_s
+    "#{left} + #{right}"
+  end
+
+  def inspect
+    "<<#{self}>>"
+  end
+
+  def reducible?
+    true
+  end
+end
+
+class Multiply < Struct.new(:left, :right)
+  def reduce(environment)
+    if left.reducible?
+      Multiply.new(left.reduce(environment), right)
+    elsif right.reducible?
+      Multiply.new(left, right.reduce(environment))
+    else
+      Number.new(left.value * right.value)
+    end
+  end
+
+  def to_s
+    "#{left} * #{right}"
+  end
+
+  def inspect
+    "<<#{self}>>"
+  end
+
+  def reducible?
+    true
+  end
+end
+
+class LessThan < Struct.new(:left, :right)
+  def reduce(environment)
+    if left.reducible?
+      LessThan.new(left.reduce(environment), right)
+    elsif right.reducible?
+      LessThan.new(left, right.reduce(environment))
+    else
+      Boolean.new(left.value < right.value)
+    end
+  end
+
+  def to_s
+    "#{left} < #{right}"
+  end
+
+  def inspect
+    "<<#{self}>>"
+  end
+
+  def reducible?
+    true
+  end
+end
+
+class Variable < Struct.new(:name)
+  def reduce(environment)
+    environment[name]
+  end
+
+  def to_s
+    name.to_s
+  end
+
+  def inspect
+    "<<#{self}>>"
+  end
+
+  def reducible?
+    true
+  end
+end
+
+class Machine < Struct.new(:expression, :environment)
+  def step
+    self.expression = expression.reduce(environment)
+  end
+
+  def run
+    while expression.reducible?
+      puts expression
+      step
+    end
+    puts expression
+  end
+end
+
+Machine.new(
+ LessThan.new(
+   Number.new(5),
+   Add.new(Number.new(2), Number.new(2))
+ ),
+ {}
+).run
+
+Machine.new(
+ Add.new(
+   Multiply.new(Number.new(1), Number.new(2)),
+   Multiply.new(Number.new(3), Number.new(4))
+ ),
+ {}
+).run
+
+Machine.new(
+  Add.new(Variable.new(:x), Variable.new(:y)),
+  { x: Number.new(3), y: Number.new(4) }
+).run
+
