ab43cd7e1490ab2e50e2cc6efb670128107306bd
3014zhangshuo <3014zhangshuo@gmail.com>
2019-09-25 07:51:43 +0800

    support assign statement

---
+++ chap2/main.rb
@@ -0,0 +1,27 @@
+require '../chap2/simple_language/main.rb'
+
+Machine.new(
+  Assign.new(:x, Add.new(Variable.new(:x), Number.new(1))),
+  { x: Number.new(2) }
+).run
+
+# Machine.new(
+#  LessThan.new(
+#    Number.new(5),
+#    Add.new(Number.new(2), Number.new(2))
+#  ),
+#  {}
+# ).run
+#
+# Machine.new(
+#  Add.new(
+#    Multiply.new(Number.new(1), Number.new(2)),
+#    Multiply.new(Number.new(3), Number.new(4))
+#  ),
+#  {}
+# ).run
+#
+# Machine.new(
+#   Add.new(Variable.new(:x), Variable.new(:y)),
+#   { x: Number.new(3), y: Number.new(4) }
+# ).run
---
+++ chap2/simple_language/main.rb
@@ -0,0 +1,5 @@
+require '../chap2/simple_language/assign.rb'
+require '../chap2/simple_language/add.rb'
+require '../chap2/simple_language/variable.rb'
+require '../chap2/simple_language/number.rb'
+require '../chap2/simple_language/machine.rb'
