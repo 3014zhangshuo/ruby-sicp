dde3dd57bc3bc6620f70692996f216673823e1f5
3014zhangshuo <3014zhangshuo@gmail.com>
2019-10-01 23:19:02 +0800

    implement while expiression

--- chap2/main.rb
+++ chap2/main.rb
@@ -39,10 +39,19 @@ require '../chap2/simple_language/main.rb'
 #   If.new(Variable.new(:x), Assign.new(:y, Number.new(1)), DoNothing.new),
 #   { x: Boolean.new(false) }
 # ).run
+#
+# Machine.new(
+#   Sequence.new(
+#     Assign.new(:x, Add.new(Number.new(1), Number.new(1))),
+#     Assign.new(:y, Add.new(Variable.new(:x), Number.new(3)))
+#   ),
+#   {}
+# ).run
+
 Machine.new(
-  Sequence.new(
-    Assign.new(:x, Add.new(Number.new(1), Number.new(1))),
-    Assign.new(:y, Add.new(Variable.new(:x), Number.new(3)))
+  While.new(
+    LessThan.new(Variable.new(:x), Number.new(5)),
+    Assign.new(:x, Multiply.new(Variable.new(:x), Number.new(3)))
   ),
-  {}
+  { x: Number.new(1) }
 ).run
---
+++ chap2/simple_language/while.rb
@@ -0,0 +1,21 @@
+require '../chap2/simple_language/do_nothing'
+require '../chap2/simple_language/if'
+require '../chap2/simple_language/sequence'
+
+class While < Struct.new(:condition, :body)
+  def to_s
+    "while (#{condition}) { #{body} }"
+  end
+
+  def inspect
+    "<<#{self}>>"
+  end
+
+  def reducible?
+    true
+  end
+
+  def reduce(environment)
+    [If.new(condition, Sequence.new(body, self), DoNothing.new), environment]
+  end
+end
